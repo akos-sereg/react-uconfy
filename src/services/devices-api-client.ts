5287688d47d94686bffa9a99aadbf4987fb62817
Akos Sereg <akos.sereg@gmail.com>
2022-03-28 21:37:04 +0200

    fetch devices from backend

--- src/services/devices-api-client.ts
+++ src/services/devices-api-client.ts
@@ -1,16 +1,23 @@
 import { Article, Device } from 'MyModels';
+import axios from 'axios';
 
 import * as localStorage from './local-storage-service';
 
 let articles: Article[] = localStorage.get<Article[]>('articles') || [];
 
 export function loadDevices(): Promise<Device[]> {
-    console.log('--> loadDevices called');
-    return new Promise((resolve, reject) => {
-        console.log('--> creating dummy device');
-        let devices: Device[] = [];
-        devices.push({ deviceID: '1233', name: 'asdf', platform: 'esp32', userID: 1234 });
-        resolve(devices);
+    return new Promise(async (resolve, reject) => {
+
+        axios.get('http://127.0.0.1:8080/api/device',
+            {
+                headers: {
+                    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxMjQwIiwiZW1haWwiOiJkZW1vdXNlckBkZW1vLmRlIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvc2lkIjoiNTkwZGRlNTktNGI4My00M2NiLThlY2UtMWM3MWQ1ZGNiN2M3Iiwicm9sZSI6ImRlbW8iLCJuYmYiOjE2NDc1NDQ3ODgsImV4cCI6MTY3OTA4MDc4OCwiaWF0IjoxNjQ3NTQ0Nzg4LCJpc3MiOiJodHRwOi8vbXlzaXRlLmNvbSIsImF1ZCI6Imh0dHA6Ly9teWF1ZGllbmNlLmNvbSJ9.bvomBIkfN3Aymum6MxQY6jdujHrCff1pn3to9jOGqo8'
+                }
+            }).then(function (response) {
+                resolve(response.data.devices);
+            }).catch(function (error) {
+                reject("Unable to fetch devices");
+            });
     });
 }
 
