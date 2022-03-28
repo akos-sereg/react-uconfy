2ec2b5628766b6a54f5ef5b0346014a6b80a3477
Akos Sereg <akos.sereg@gmail.com>
2022-03-28 23:43:46 +0200

    refact

--- src/features/articles/components/ArticleListItem.tsx
+++ src/features/articles/components/ArticleListItem.tsx
@@ -3,13 +3,7 @@ import React from 'react';
 import areEqual from 'fast-deep-equal';
 import { connect } from 'react-redux';
 
-// import { deleteArticleAsync } from '../actions';
-import { getPath } from '../../../router-paths';
-import FlexRow from '../../../components/FlexRow';
-import { Link } from 'react-router-dom';
-
 const dispatchProps = {
-  // deleteArticle: deleteArticleAsync.request,
   deleteArticle: (device: Device) => {},
 };
 
@@ -19,27 +13,12 @@ type Props = typeof dispatchProps & {
 
 const ArticleListItem = React.memo<Props>(({ device, deleteArticle }) => {
   return (
-    <FlexRow>
-      <div style={getStyle()}>{device.name}</div>
-      <FlexRow itemsSpacing={20}>
-        <Link to={getPath('viewArticle', device.deviceID)}>View</Link>
-        <div
-          className="link"
-          onClick={() => deleteArticle(device)}
-          style={{ color: 'darkred' }}
-        >
-          Delete
-        </div>
-      </FlexRow>
-    </FlexRow>
+    <>
+        {device.name}
+    </>
   );
 }, areEqual);
 
-const getStyle = (): React.CSSProperties => ({
-  overflowX: 'hidden',
-  textOverflow: 'ellipsis',
-  width: '300px',
-});
 
 export default connect(
   null,
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import ArticleList from '../features/articles/components/ArticleList';
-import ArticleActionsMenu from '../features/articles/components/ArticleActionsMenu';
 import Main from '../layouts/Main';
 
 export default () => (
-  <Main renderActionsMenu={() => <ArticleActionsMenu />}>
-    List of articles
+  <Main>
     <ArticleList />
   </Main>
 );
