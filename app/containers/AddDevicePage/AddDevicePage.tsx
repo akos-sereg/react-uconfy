4bd7c2a1aec12e6f9573f9311bcdaf3dc3fd712a
Akos Sereg <akos.sereg@gmail.com>
2022-06-07 21:49:00 +0200

    sitebuild for create device page

--- app/containers/AddDevicePage/AddDevicePage.tsx
+++ app/containers/AddDevicePage/AddDevicePage.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthorList from '../../components/AuthorList'
@@ -17,11 +17,35 @@ type Props = {
 };
 
 const AddDevicePage = (props: Props) => {
+  const nameEl = useRef(null);
+  const platformEl = useRef(null);
+
+  const handleCreate = (event: any) => {
+    event.preventDefault()
+    const name = nameEl.current.value
+    const platform = platformEl.current.value
+  }
 
   return (
-      <>
-        ehlo
-      </>
+      <div className={styles.createDeviceContainer}>
+
+        <h2>Create new Device</h2>
+        <p>
+          Once the device is registered, an API key will be generated, so your device can send data safely.
+        </p>
+
+        <div className={`input-group ${styles.formItem}`}>
+          <span className={`input-group-addon ${styles.fieldName}`} id="name">Name</span>
+          <input type="text" ref={nameEl} className={`form-control ${styles.fieldValue}`} placeholder="Name" aria-describedby="name" />
+        </div>
+
+        <div className={`input-group ${styles.formItem}`}>
+          <span className={`input-group-addon ${styles.fieldName}`} id="platform">Platform</span>
+          <input type="text" ref={platformEl} className={`form-control ${styles.fieldValue}`} placeholder="Platform" aria-describedby="platform" />
+        </div>
+
+        <button type="button" className="btn btn-primary" onClick={handleCreate}>Create</button>
+      </div>
     );
 }
 
