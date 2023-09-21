48a0e53a16dcfd84dc212da1b4061c22f665b0d3
Akos Sereg <akos.sereg@gmail.com>
2023-09-21 18:58:54 +0200

    device listing based on categories

--- app/containers/DeviceListPage/DeviceListPage.tsx
+++ app/containers/DeviceListPage/DeviceListPage.tsx
@@ -7,6 +7,7 @@ import {fetechDevices} from "./actions";
 import styles from './style.scss'
 import {setNavigation} from "../../components/Navigation/actions";
 import {getMarketingPage} from "../../services/UrlService";
+import { DeviceDetails } from "../../model/DeviceDetails";
 
 type Props = {
   dispatch: any,
@@ -24,7 +25,26 @@ const DeviceListPage = (props: Props) => {
       { name: 'uConfy', uri: getMarketingPage() },
       { name: 'Devices' },
     ]));
-  }, [])
+  }, []);
+
+  const getLastSeenCategory = (deviceId: string) => {
+    if (!props.devicesData.deviceIdLastSeen) {
+      return ''
+    }
+
+    let lastSeen = null
+    if (props.devicesData.deviceIdLastSeen[deviceId]) {
+      lastSeen = props.devicesData.deviceIdLastSeen[deviceId];
+    }
+
+    if (lastSeen < 86400 && lastSeen !== -1) {
+      return 'active';
+    } else if (lastSeen >= 86400) {
+      return 'inactive';
+    } else if (lastSeen === -1) {
+      return 'never-seen';
+    }
+  };
 
   return (
       <>
@@ -38,12 +58,23 @@ const DeviceListPage = (props: Props) => {
           </div>
         }
 
-
         {props.devicesData && <DeviceList
-          items={props.devicesData.devices}
+          items={props.devicesData.devices.filter((device: DeviceDetails) => getLastSeenCategory(device.deviceID) == 'active')}
           lastSeen={props.devicesData.deviceIdLastSeen}
           dispatch={props.dispatch}
           />}
+
+        {props.devicesData && <DeviceList
+          items={props.devicesData.devices.filter((device: DeviceDetails) => getLastSeenCategory(device.deviceID) == 'inactive')}
+          lastSeen={props.devicesData.deviceIdLastSeen}
+          dispatch={props.dispatch}
+        />}
+
+        {props.devicesData && <DeviceList
+          items={props.devicesData.devices.filter((device: DeviceDetails) => getLastSeenCategory(device.deviceID) == 'never-seen')}
+          lastSeen={props.devicesData.deviceIdLastSeen}
+          dispatch={props.dispatch}
+        />}
 
         <AddDeviceItem dispatch={props.dispatch} />
       </>
